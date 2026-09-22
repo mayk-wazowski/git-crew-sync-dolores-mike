d7bca3f91e311aa2ad83be01cb2a209d805b3b5f
maykwazowski <mikejuneild@gmail.com>
2026-09-22 22:24:29 +0800

    Round shift pay instead of truncating - dolores.mike

--- shifts.js
+++ shifts.js
@@ -1,9 +1,13 @@
 function isValidShift(hours) {
+
   return hours > 0 && hours <= 24;
+
 }
 
 function calculatePay(hours, rate) {
-  return Math.floor(hours * rate);
+
+  return Math.round(hours * rate);
+
 }
 
 module.exports = { isValidShift, calculatePay };
