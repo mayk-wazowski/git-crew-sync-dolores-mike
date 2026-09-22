4e05b77a5347436764e47ad798afa33b03fc486d
maykwazowski <mikejuneild@gmail.com>
2026-09-22 23:17:39 +0800

    Fix pay calculation tests - dolores.mike

--- shifts.js
+++ shifts.js
@@ -1,7 +1,5 @@
 function isValidShift(hours) {
-
   return hours > 0 && hours <= 24;
-
 }
 
 function calculatePay(hours, rate) {
@@ -11,11 +9,10 @@ function calculatePay(hours, rate) {
   }
 
   if (hours > 8) {
-    return Math.round((8 * rate) + ((hours - 8) * rate * 1.5));
+    return Math.floor((8 * rate) + ((hours - 8) * rate * 1.5));
   }
 
-  return Math.round(hours * rate);
-
+  return Math.floor(hours * rate);
 }
 
 module.exports = { isValidShift, calculatePay };
